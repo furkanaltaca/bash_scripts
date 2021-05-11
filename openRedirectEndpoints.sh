335e1138f92ec854da60909d7dfbe2ff03123076
furkanaltaca <furkanaltaca@gmail.com>
2021-05-12 00:45:19 +0300

    add openRedirectEndpoints.sh, edit subListScreenshot.sh

---
+++ openRedirectEndpoints.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+domain=$1
+mainPath="/root/recon/$1"
+
+mkdir -p $mainPath
+
+python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt | cat $mainPath/ragno_urls.txt | grep -a -i \=http | qsreplace "http://google.com" | while read target_url do; do curl -s -L $target_url -I | grep "google.com" && echo "[+] [Vulnerable] $target_url \n"; done
+
