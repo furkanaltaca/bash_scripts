8498298e8589b1c20c42ebc89648ee31f9d43189
furkanaltaca <furkanaltaca@gmail.com>
2021-05-12 01:06:52 +0300

    edit

--- openRedirectEndpoints.sh
+++ openRedirectEndpoints.sh
@@ -5,8 +5,9 @@ mainPath="/root/recon/$domain"
 
 mkdir -p $mainPath
 
-python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt \
-| cat $mainPath/ragno_urls.txt \
+python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt
+
+cat $mainPath/ragno_urls.txt \
 | grep -a -i \=http \
 | qsreplace "http://google.com" \
 | while read target_url do; \
