b86de2b4ed1710c1dfc5e87eff94176254b9fedf
furkanaltaca <furkanaltaca@gmail.com>
2021-05-11 14:12:34 +0300

    edit

--- subListScreenshot.sh
+++ subListScreenshot.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-mainPath="recon/$1"
-mkdir ~/$mainPath
-python3 ~/tools/Sublist3r/sublist3r.py -d $1 -o ~/$mainPath/subdomains.txt 
-cat ~/$mainPath/subdomains.txt | httprobe > ~/$mainPath/httprobe.txt
-cat ~/$mainPath/httprobe.txt | grep https:// > ~/$mainPath/https.txt
-cat ~/$mainPath/httprobe.txt | grep http:// > ~/$mainPath/http.txt
-python3 ~/tools/EyeWitness/Python/EyeWitness.py -f ~/$mainPath/https.txt --web -d ~/$mainPath/eyewitness/https --no-prompt
-python3 ~/tools/EyeWitness/Python/EyeWitness.py -f ~/$mainPath/http.txt --web -d ~/$mainPath/eyewitness/http --no-prompt
+domain=$1
+mainPath="/root/recon/$1"
+mkdir -p $mainPath
 
-echo "done. path: ~/$mainPath/eyewitness/"
+python3 ~/tools/Sublist3r/sublist3r.py -d $domain -o $mainPath/subdomains.txt
+
+cat $mainPath/subdomains.txt | httprobe > $mainPath/httprobe.txt
+cat $mainPath/httprobe.txt | grep https:// > $mainPath/https.txt
+cat $mainPath/httprobe.txt | grep http:// > $mainPath/http.txt
+
+mkdir -p $mainPath/eyewitness
+python3 ~/tools/EyeWitness/Python/EyeWitness.py -f $mainPath/https.txt -d $mainPath/eyewitness/https --web --no-prompt
+python3 ~/tools/EyeWitness/Python/EyeWitness.py -f $mainPath/http.txt -d $mainPath/eyewitness/http --web --no-prompt
+
+echo "done. path: $mainPath/eyewitness/"
+
