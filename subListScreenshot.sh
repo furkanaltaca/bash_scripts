68067a5095588ae48b2b53841d9e8d922386ac7e
furkanaltaca <41112774+furkanaltaca@users.noreply.github.com>
2021-05-11 13:05:11 +0300

    Update subListScreenshot.sh

--- subListScreenshot.sh
+++ subListScreenshot.sh
@@ -6,5 +6,7 @@ python3 ~/tools/Sublist3r/sublist3r.py -d $1 -o ~/$mainPath/subdomains.txt
 cat ~/$mainPath/subdomains.txt | httprobe > ~/$mainPath/httprobe.txt
 cat ~/$mainPath/httprobe.txt | grep https:// > ~/$mainPath/https.txt
 cat ~/$mainPath/httprobe.txt | grep http:// > ~/$mainPath/http.txt
-python3 ~/tools/EyeWitness/Python/EyeWitness.py -f ~/$mainPath/https.txt --web -d ~/$mainPath/eyewitness/https
-python3 ~/tools/EyeWitness/Python/EyeWitness.py -f ~/$mainPath/http.txt --web -d ~/$mainPath/eyewitness/http
+python3 ~/tools/EyeWitness/Python/EyeWitness.py -f ~/$mainPath/https.txt --web -d ~/$mainPath/eyewitness/https --no-prompt
+python3 ~/tools/EyeWitness/Python/EyeWitness.py -f ~/$mainPath/http.txt --web -d ~/$mainPath/eyewitness/http --no-prompt
+
+echo "done. path: ~/$mainPath/eyewitness/"
