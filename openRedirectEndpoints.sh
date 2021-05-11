f4da10fbbe18a43843b5d5314e86b3720debdadf
furkanaltaca <41112774+furkanaltaca@users.noreply.github.com>
2021-05-12 00:58:17 +0300

    Update openRedirectEndpoints.sh

--- openRedirectEndpoints.sh
+++ openRedirectEndpoints.sh
@@ -5,5 +5,12 @@ mainPath="/root/recon/$1"
 
 mkdir -p $mainPath
 
-python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt | cat $mainPath/ragno_urls.txt | grep -a -i \=http | qsreplace "http://google.com" | while read target_url do; do curl -s -L $target_url -I | grep "google.com" && echo "[+] [Vulnerable] $target_url \n"; done
+python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt \
+| cat $mainPath/ragno_urls.txt \
+| grep -a -i \=http \
+| qsreplace "http://google.com" \
+| while read target_url do; \
+do curl -s -L $target_url -I \
+| grep "google.com" \
+&& echo "[+] [Vulnerable] $target_url \n"; done
 
