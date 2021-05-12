27ecade0efef14b19dde300a0ba2cadd5fd55518
furkanaltaca <furkanaltaca@gmail.com>
2021-05-12 18:18:58 +0300

    edit openRedirect script

--- openRedirectEndpoints.sh
+++ openRedirectEndpoints.sh
@@ -5,22 +5,23 @@ mainPath="/root/recon/$domain"
 
 mkdir -p $mainPath
 
+deleteTempFiles(){
 rm -f $mainPath/ragno_urls.txt
-rm -f $mainPath/qsreplace.txt
+}
+
+deleteTempFiles
 
 python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt
 
 cat $mainPath/ragno_urls.txt \
 | grep -a -i \=http \
-| qsreplace "http://google.com" > $mainPath/qsreplace.txt
-
-while read target_url
+| qsreplace "http://google.com" \
+| while read target_url
 do
 if curl -s -L $target_url -I | grep -e "google.com" -e "HTTP" | grep -q "301\|302"
 then
  echo $target_url
 fi
-done < $mainPath/qsreplace.txt
+done
 
-rm -f $mainPath/ragno_urls.txt
-rm -f $mainPath/qsreplace.txt
+deleteTempFiles
