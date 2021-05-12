06deb45cce17b9252f89ace6c2d7aab547769204
furkanaltaca <furkanaltaca@gmail.com>
2021-05-12 14:50:28 +0300

    edit openRedirect script

--- openRedirectEndpoints.sh
+++ openRedirectEndpoints.sh
@@ -7,11 +7,12 @@ mkdir -p $mainPath
 
 python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt
 
+rm -f $mainPath/openRedirectEndpoints.txt
 cat $mainPath/ragno_urls.txt \
 | grep -a -i \=http \
 | qsreplace "http://google.com" \
 | while read target_url do; \
 do curl -s -L $target_url -I \
 | grep "google.com" \
-&& echo "[+] [Vulnerable] $target_url \n"; done
-
+| grep -e "location" -e "Location" \
+>> $mainPath/openRedirectEndpoints.txt; done
