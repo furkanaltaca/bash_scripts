66f683edd775afde253ce70db4792a79ef1ce516
furkanaltaca <41112774+furkanaltaca@users.noreply.github.com>
2021-05-12 17:23:41 +0300

    Update openRedirectEndpoints.sh

--- openRedirectEndpoints.sh
+++ openRedirectEndpoints.sh
@@ -5,10 +5,11 @@ mainPath="/root/recon/$domain"
 
 mkdir -p $mainPath
 
-python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt
-
+rm -f $mainPath/ragno_urls.txt
 rm -f $mainPath/openRedirectEndpoints.txt
 rm -f $mainPath/qsreplace.txt
+
+python3 ~/tools/ragno/ragno.py -d $domain -q -o $mainPath/ragno_urls.txt
 
 cat $mainPath/ragno_urls.txt \
 | grep -a -i \=http \
