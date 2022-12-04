cc1d5824f194978cfb5cae21c11a1374b7c46315
furkanaltaca <furkanaltaca@gmail.com>
2022-12-04 14:16:30 +0300

    Add crtsh subdomains gathering feature for sub-enum-auto.sh

--- sub-enum-auto.sh
+++ sub-enum-auto.sh
@@ -16,13 +16,20 @@ cd $working_dir
 cp Sudomy-Output/$domain/subdomain.txt sudomy.txt &&
 sudo rm -rf Sudomy-Output
 
+#crtsh
+curl -i -k "https://crt.sh/?q=%25.$domain" | html2text > crtsh-response.txt &&
+cat crtsh-response.txt | grep $domain | awk -F " " '{print $5}' | sort -u > crtsh.txt &&
+sudo rm -rf crtsh-response.txt
+
 # merge and output all subdomains with httprobe
-cat assetfinder.txt subfinder.txt sudomy.txt | sort -u > subdomains.txt &&
+cat assetfinder.txt subfinder.txt sudomy.txt crtsh.txt | sort -u > subdomains.txt &&
 cat subdomains.txt | httprobe > httprobe.txt &&
 
-sudo rm -rf assetfinder.txt subfinder.txt sudomy.txt
+sudo rm -rf assetfinder.txt subfinder.txt sudomy.txt crtsh.txt
 
 mkdir $domain
 mv subdomains.txt httprobe.txt $domain
 
 echo -e "$(cat subdomains.txt|wc -l) domains found. \n Directory: ./$domain \n Finished."
+
+
