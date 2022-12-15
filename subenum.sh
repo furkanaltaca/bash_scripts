3984d1d0b30f8fc782abaa423b9e37e9bb27ca12
furkanaltaca <furkanaltaca@gmail.com>
2022-12-16 00:45:58 +0300

    Update subdomain enumeration tool

--- subenum.sh
+++ subenum.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# subenum is a subdomain enumeration automate script
+
 domain=$1
 working_dir=$(pwd)
 
@@ -9,22 +11,15 @@ assetfinder $domain > assetfinder.txt &&
 # subfinder
 subfinder -silent -d $domain -o subfinder.txt >/dev/null &&
 
-# sudomy
-cd /opt/Sudomy &&
-sudo ./sudomy -d $domain -o $working_dir --no-probe >/dev/null&&
-cd $working_dir
-cp Sudomy-Output/$domain/subdomain.txt sudomy.txt &&
-sudo rm -rf Sudomy-Output
-
 #crtsh
 curl -i -k "https://crt.sh/?q=%25.$domain" | html2text > crtsh-response.txt &&
 cat crtsh-response.txt | awk -F " " '{print $5}' | grep $domain | sort -u > crtsh.txt &&
-sudo rm -rf crtsh-response.txt
+rm crtsh-response.txt
 
 # append all subdomains in a file
-cat assetfinder.txt subfinder.txt sudomy.txt crtsh.txt | sort -u > subdomains.txt &&
+cat assetfinder.txt subfinder.txt crtsh.txt | sort -u > subdomains.txt &&
 
-sudo rm -rf assetfinder.txt subfinder.txt sudomy.txt crtsh.txt
+rm assetfinder.txt subfinder.txt crtsh.txt
 
 echo -e "$(cat subdomains.txt|wc -l) domains found. \n Directory: ./subdomains.txt \n Finished."
 
